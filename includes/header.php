db1ebb3b57f27d1a2e5b3f46a2d50cc07f2653bf
lemasani <brianlemasan@gmail.com>
2023-08-10 09:30:13 +0300

    created login and header

---
+++ includes/header.php
@@ -0,0 +1,46 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tanesco-Security</title>
+
+    <!-- custom css -->
+    
+    <style><?php include "..\Assets\css\header.css"; ?></style>
+    <!-- bootstrap link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+  <body>
+    <header class="header">
+    <nav class="navbar ">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+            <img src="#" alt="Bootstrap" width="30" height="24">
+            </a>
+          
+           
+            <a href="../index.php" class="brand-name">Tanesco</a>
+        </div>
+    </nav>
+    </header>
+
+
+
+
+
+
+
+    <!-- bootstrap script link -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
+<?php
+
+if(isset($_POST["tanesco"]))
+
+header("location: index.php")
+?>
--- index.php
+++ index.php
@@ -5,16 +5,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>NEWOONE</title>
     <!-- custom css -->
-    <link rel="stylesheet" href="Assets/css/main.css">
-
+    
+    <style><?php include "Assets\css\main.css"; ?></style>
     <!-- bootstrap css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello, world!</h1>
-    <button class="btn btn-primary">click me!</button>
+    <div class="form-wrap">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+          <form class="login-form" action="" method="post">
+              <div class="container">
+              <h3>Tanesco-Security</h3>
+                  <div class="row">
+                      
+                          <input type="text" placeholder="Employee ID">                                              
+                     
+                  </div>
+                  <div class="row">
+                      
+                        <input type="password" name="password" id="spassword" placeholder="Password">
+                    
+                  </div>
+                  <div class="row">
+                   
+                      <button class="btn-login">Login</button>
+                  
+                  </div>
+                    <a href="#" class="forget-password">forgot password</a>
+              </div>
+          </form>
+          </div>
+        </div>
+         
 
-
+      </div>
+    </div>
+    
+   
     <!-- bootstrap js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
   </body>
