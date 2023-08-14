46672f27493354dcfb0cdcd4d9ef37375771863f
lemasani <brianlemasan@gmail.com>
2023-08-14 12:23:33 +0300

    update sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -16,6 +16,46 @@ session_start();
     <!-- custom css -->
     
     <style><?php include "..\Assets\css\sidebar.css"; ?></style>
+    <style>
+            .sidenav {
+        height: 100%;
+        width: 0;
+        position: fixed;
+        z-index: 3;
+        top: 0;
+        left: 0;
+        background-color: #1A5D1A;
+        overflow-x: hidden;
+        transition: 0.5s;
+        padding-top: 60px;
+        }
+
+        .sidenav a {
+        padding: 8px 8px 8px 32px;
+        text-decoration: none;
+        font-size: 25px;
+        color: white;
+        display: block;
+        transition: 0.3s;
+        }
+
+        .sidenav a:hover {
+        color: #f1f1f1;
+        }
+
+        .sidenav .closebtn {
+        position: absolute;
+        top: 0;
+        right: 25px;
+        font-size: 36px;
+        margin-left: 50px;
+        }
+
+        @media screen and (max-height: 450px) {
+        .sidenav {padding-top: 15px;}
+        .sidenav a {font-size: 18px;}
+        }
+</style>
     <!-- bootstrap link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
@@ -23,82 +63,30 @@ session_start();
   <body>
   <?php include "header.php"; ?>
   <div class="sidebar ">
-  <div class="container-fluid">
-    <div class="row flex-nowrap">
-        <div class="col-auto col-md-2 col-lg-1 px-sm-1 px-0 bg-success"  >
-            <div class="d-flex flex-column align-items-center align-items-sm-start px-2 pt-2 text-white min-vh-100 ">
-            
-                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link align-middle text-white px-0 ">
-                        <i class="bi bi-slash-square-fill"></i><span class="ms-1 d-none d-sm-inline">Status</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle text-white">
-                        <i class="bi bi-exclamation-square"></i> <span class="ms-1 d-none d-sm-inline">Incident</span> </a>
-                        <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-                            <li class="w-100">
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline text-white ">New</span></a>
-                            </li>
-                            
-                        </ul>
-                    </li>
-                   
-                    <li>
-                        <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle text-white ">
-                            <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Projects</span> </a>
-                            <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
-                            <li class="w-100">
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline text-white">New Project</span></a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline text-white">Ongoing</span></a>
-                            </li>
-                            
-                        </ul>
-                    </li>
-
-                    <li>
-                    <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle text-white">
-                    <i class="bi bi-door-closed"></i><span class="ms-1 d-none d-sm-inline">Sign out</span> </a>
-                    
-                    </li>
-
-                </ul>
-                <hr>
-                <div class="dropdown pb-4">
-                    <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
-                        <span class="d-none d-sm-inline mx-1">User</span>
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-                        <li><a class="dropdown-item" href="#">New project...</a></li>
-                        <li><a class="dropdown-item" href="#">Settings</a></li>
-                        <li><a class="dropdown-item" href="#">Profile</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Sign out</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="col py-3">
-           content area
-
-        
-        </div>
-    </div>
+  <div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#">Status</a>
+  <a href="#">Incidenct</a>
+  <a href="#">Project</a>
+  <a href="#">Sign Out</a>
 </div>
+
+<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
   </div>
   
 
 
 
 
+  <script>
+    function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    }
 
-
+    function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    }
+</script>
     <!-- bootstrap script link -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
   </body>
