ba2dc39a280b61b510da225edd118dadec5d83c9
lemasani <brianlemasan@gmail.com>
2023-08-21 10:47:12 +0300

    validation styling

--- action/incident.php
+++ action/incident.php
@@ -19,6 +19,89 @@
   <body>
 
   <?php include "..\includes\header.php"; ?>
+  <?php
+ $msg = '';
+ $title = '';
+$incident = '';
+$description = ''; 
+$region = '';
+$district = '';
+$street  = '';
+
+if($_SERVER['REQUEST_METHOD']=='POST') {
+
+
+$title = $_POST['title'];
+$incident = $_POST['incident'];
+$description = $_POST['description'];
+$region = $_POST['region'];
+$district= $_POST['district'];
+$street = $_POST['street'];
+
+
+if(empty($title) )
+{
+global $msg;
+$msg = '<div class="alert alert-danger mt-2 text-center">fill the title first</div>';
+}
+
+elseif(empty($incident))
+{
+  global $msg;
+$msg = '<div class="alert alert-danger mt-2 text-center">Enter the incident type</div>';
+}
+elseif(empty($description))
+{
+global $msg;
+$msg = '<div class="alert alert-danger mt-2 text-center">fill in the description</div>';
+}
+elseif(empty($region))
+{
+  global $msg;
+$msg = '<div class="alert alert-danger mt-2 text-center">Enter region</div>';
+}
+elseif(empty($district))
+ {
+  global $msg;
+$msg = '<div class="alert alert-danger mt-2 text-center">Enter district</div>';
+}
+elseif(empty($street))
+{
+  global $msg;
+$msg = '<div class="alert alert-danger mt-2 text-center">Enter street or ward</div>';
+}
+
+
+
+
+else
+{
+  
+$sql = "insert into `incident_db`(title,incident,description,region,district,street)
+values('$title','$incident',' $description','$region','$district','$street')";
+
+
+
+$result = mysqli_query($con,$sql);
+
+if($result)
+{
+
+   $title = $_POST['title'];
+   $incident = $_POST['incident'];
+   $description = $_POST['description'];
+   $region = $_POST['region'];
+   $district= $_POST['district'];
+   $street = $_POST['street'];
+
+   
+   
+$msg = '<div class="alert alert-success mt-2 text-center">Data sent successfully</div>';
+
+}
+}
+}
+?>
   
   <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post" class="incident-form">
     <div class="container">
@@ -30,11 +113,11 @@
             <div class="row">
               <div class="col-md-6 col-lg-6 d-flex flex-column">
                 <label for="title">Title</label>
-                <input type="text" name="title" id="title">
+                <input type="text" name="title" id="title" value=" <?php echo $title ?>">
               </div>
               <div class="col-md-6 col-lg-6 d-flex flex-column">
                   <label for="type">Type</label>
-                  <select name="incident" id="type"> 
+                  <select name="incident" id="type" value=" <?php echo  $incident ?>"> 
                     <option value=""></option>           
                     <option value="damages">Damages</option>
                     <option value="theft">Theft</option>
@@ -45,22 +128,22 @@
             <div class="row">
               <div class="col-md-12 d-flex flex-column">
                 <label for="description">Description</label>
-                <textarea name="description" id="description" class="w-100" placeholder="Enter description"></textarea>
+                <textarea name="description" id="description" class="w-100" placeholder="Enter description" value=" <?php echo $description ?>"></textarea>
               </div>
             </div>
 
             <div class="row">
               <div class="col-md-3 d-flex flex-column">
                 <label for="region">Region:</label>
-                <input type="text" id="region" name="region" >
+                <input type="text" id="region" name="region" value="<?php echo $region ?>" >
               </div>
               <div class="col-md-3 d-flex flex-column">
                 <label for="district">District</label>
-                <input type="text" id="district" name="district" >
+                <input type="text" id="district" name="district" value=" <?php echo $district ?>" >
               </div>
               <div class="col-md-3 d-flex flex-column">
                 <label for="street">Street/ward</label>
-                <input type="text" id="street"name="street">
+                <input type="text" id="street"name="street" value=" <?php echo $street ?>">
               </div>
             </div>
             <fieldset class="offender-details p-2">
@@ -101,6 +184,12 @@
                 <input type="submit" value="submit" name="submit" class="btn-submit w-100">
               </div>
             </div>
+            <div class="row">
+              <div class="col-md-12">
+                
+              <?php echo $msg; ?>
+              </div>
+            </div>
     </div>
             
 
@@ -115,70 +204,3 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
   </body>
 </html>
-<?php
-if($_SERVER['REQUEST_METHOD']=='POST') {
-
-
-$title = $_POST['title'];
-$incident = $_POST['incident'];
-$description = $_POST['description'];
-$region = $_POST['region'];
-$district= $_POST['district'];
-$street = $_POST['street'];
-
-
-
-if(empty($title) )
-{
-echo"fill the title first";
-}
-
-elseif(empty($incident))
-{
-  echo"provide the incident type";
-}
-elseif(empty($description))
-{
-echo"fill in the description";
-}
-elseif(empty($region))
-{
-  echo"provide region";
-}
-elseif(empty($district))
- {
-  echo"provide district";
-}
-elseif(empty($street))
-{
-  echo"provide street/ward";
-}
-
-
-
-
-else
-{
-  
-$sql = "insert into `incident_db`(title,incident,description,region,district,street)
-values('$title','$incident',' $description','$region','$district','$street')";
-
-
-
-$result = mysqli_query($con,$sql);
-
-if($result)
-{
-
-   $title = $_POST['title'];
-   $incident = $_POST['incident'];
-   $description = $_POST['description'];
-   $region = $_POST['region'];
-   $district= $_POST['district'];
-   $street = $_POST['street'];
-
-   echo"data inserted";
-}
-}
-}
-?>
