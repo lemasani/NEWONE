f013dbb97005bc0825d5ecdfa41733f605bb2b6e
lemasani <brianlemasan@gmail.com>
2023-08-21 14:27:58 +0300

    header componet for contractor

---
+++ includes/header1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container-fluid" style="margin-top:5px;">
+                <div class="row">
+
+                <nav class="col-sm-2 bg-light sidebar py-5 d-print-none"><!-- start side bar -->
+                     <div class="sidebar-sticky">
+                     <ul class="nav flex-column">
+                     <li class="nav-item"><a class="nav-link" href="UserProfile.php"><i class="fas fa-user-alt"></i>Profile</a>
+                     </li>
+                     <li class="nav-item"><a class="nav-link" href="mechanic.php"><i class="fas fa-users"></i>Mechanics</a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="submitrequest.php"><i class="fas fa-envelope"></i>Submit Request</a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="responded.php"><i class="fas fa-check-double"></i>Responded Requests</a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="chekstatus.php"><i class="fas fa-envelope-open"></i>Check Status</a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="userchangepass.php"><i class="fas fa-key"></i>Change Password</a>
+                     </li>
+                     <li class="nav-item"><a class="nav-link" href="feedback.php"><i class="fab fa-accessible-icon"></i>Feedback</a>
+                     </li>
+                     <li class="nav-item"><a class="nav-link" href="payment.php"><i class="fas fa-dollar-sign"></i>Payments</a>
+                            </li>
+                    <li class="nav-item"><a class="nav-link" href="logout.php"><i class="fas fa-sign-out-alt"></i>Log out</a>
+                    </li>
+                    </ul>
+                 </div>
+             </nav>
+</body>
+</html>
