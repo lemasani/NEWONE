8df00aec8482786f9c2f9aced66bcf176362afe0
lemasani <brianlemasan@gmail.com>
2023-08-21 15:36:44 +0300

    including sidebar

--- includes/header1.php
+++ includes/header1.php
@@ -1,37 +1,49 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<div class="container-fluid" style="margin-top:5px;">
-                <div class="row">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tanesco-Security</title>
+    <!-- custom css -->
+    <style>
+        .sidebar{
+            height: 90vh;
+            position: relative;
+            top: 100px;
+        }
+       
+    </style>
+        
+    <style><?php include "..\Assets\css\header.css"; ?></style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+  <body>
+    <?php include "header.php" ?>
+    <div class="container-fluid" style="margin-top:5px;">
+            <div class="row">
 
-                <nav class="col-sm-2 bg-light sidebar py-5 d-print-none"><!-- start side bar -->
-                     <div class="sidebar-sticky">
+                <nav class="col-sm-2 bg-light sidebar py-5 px-3"><!-- start side bar -->
+                     <div class="sidebar-sticky align-center">
                      <ul class="nav flex-column">
-                     <li class="nav-item"><a class="nav-link" href="UserProfile.php"><i class="fas fa-user-alt"></i>Profile</a>
+                     <li class="nav-item"><a class="nav-link text-success p-2" href="#"><i class="bi bi-card-text me-2"></i>Dashboard</a>
                      </li>
-                     <li class="nav-item"><a class="nav-link" href="mechanic.php"><i class="fas fa-users"></i>Mechanics</a>
+                     <li class="nav-item"><a class="nav-link text-success p-2" href="#"><i class="bi bi-people me-2"></i>Workforce</a>
                     </li>
-                    <li class="nav-item"><a class="nav-link" href="submitrequest.php"><i class="fas fa-envelope"></i>Submit Request</a>
+                    <li class="nav-item"><a class="nav-link text-success p-2" href="#"><i class="bi bi-app-indicator me-2"></i>Response</a>
                     </li>
-                    <li class="nav-item"><a class="nav-link" href="responded.php"><i class="fas fa-check-double"></i>Responded Requests</a>
+                    <li class="nav-item"><a class="nav-link text-success p-2" href="#"><i class="bi bi-flag me-2"></i>Issues</a>
                     </li>
-                    <li class="nav-item"><a class="nav-link" href="chekstatus.php"><i class="fas fa-envelope-open"></i>Check Status</a>
-                    </li>
-                    <li class="nav-item"><a class="nav-link" href="userchangepass.php"><i class="fas fa-key"></i>Change Password</a>
-                     </li>
-                     <li class="nav-item"><a class="nav-link" href="feedback.php"><i class="fab fa-accessible-icon"></i>Feedback</a>
-                     </li>
-                     <li class="nav-item"><a class="nav-link" href="payment.php"><i class="fas fa-dollar-sign"></i>Payments</a>
-                            </li>
-                    <li class="nav-item"><a class="nav-link" href="logout.php"><i class="fas fa-sign-out-alt"></i>Log out</a>
+                    
+                    <li class="nav-item"><a class="nav-link text-success p-2" href="#"><i class="bi bi-box-arrow-left me-2"></i>Log out</a>
                     </li>
                     </ul>
-                 </div>
-             </nav>
-</body>
+                    </div>
+                </nav>
+            </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
 </html>
+
+
