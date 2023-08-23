554e187c0f88d4459fbc4befe632da5fac8a60e7
lemasani <brianlemasan@gmail.com>
2023-08-23 16:30:41 +0300

    inserted table in a form

---
+++ contractor/workforce.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Workforce</title>
+
+    <style>
+      .worforce-details{
+       
+        position: relative;
+        top:-570px;
+        margin-left: 400px;
+        z-index: 1;
+        width: 70%;
+      }
+     
+
+    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+  <body>
+  <?php include "..\includes\header1.php" ?>
+  
+    <div class="worforce-details">
+        <div class="row">
+            <div class="project-details">
+                <h4>Project details</h4>
+                <div class="row d-flex my-2">
+                    <div class="col-md-4 d-flex flex-column">
+                        <label for="project">Project name</label>
+                        <input type="text" id="project" >
+                    </div>
+        
+                    <div class="col-md-4 d-flex flex-column">
+                        <label for="jurisdiction">jurisdiction</label>
+                        <input type="text"id="jurisdiction" >
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3 d-flex flex-column">
+                        <label for="">Region</label>
+                        <input type="text">
+                    </div>
+                
+                    <div class="col-md-3 d-flex flex-column">
+                        <label for="">District</label>
+                        <input type="text" >
+                    </div>
+                    <div class="col-md-3 d-flex flex-column">
+                        <label for="">Street</label>
+                        <input type="text">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <h4 class="title my-3">Worforce Details</h4>
+            <div class="row">
+                <form action="" method="post">
+                    
+                </form>
+            </div>
+        </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
