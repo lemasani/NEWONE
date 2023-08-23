192c79b5ff5c4dc486ae6a91975169014b74cb7a
lemasani <brianlemasan@gmail.com>
2023-08-23 16:32:00 +0300

    inserted table into form

--- contractor/workforce.php
+++ contractor/workforce.php
@@ -58,13 +58,72 @@
         <div class="row">
             <h4 class="title my-3">Worforce Details</h4>
             <div class="row">
+            <INPUT type="button" value="Add Row" onClick="addRow('dataTable')" />
+
+            <INPUT type="button" value="Delete Row" onClick="deleteRow('dataTable')" />
                 <form action="" method="post">
-                    
+                <TABLE width="425" border="1">
+                <thead>
+                <tr>
+                <th width="98"></th>
+                <th width="94">NIDA</th>
+                <th width="121">Name</th>
+                <th width="84">Phone No</th>
+
+                </tr>
+                </thead>
+                <tbody id="dataTable">
+
+                </tbody>
+                </TABLE>
+
+                <INPUT type="submit" value="Insert" name="submit" />
                 </form>
             </div>
         </div>
     </div>
-    
+    <script>
+        function addRow(tableID) { 
+
+        var table = document.getElementById(tableID);
+
+        var rowCount = table.rows.length;
+        var row = table.insertRow(rowCount);
+
+        var cell1 = row.insertCell(0);
+        var element1 = document.createElement("input");
+        element1.type = "checkbox";
+        element1.name="chkbox[]";
+        cell1.appendChild(element1);
+
+        var cell2 = row.insertCell(1);
+        cell2.innerHTML = "<input type='text' name='nida'>";
+
+        var cell3 = row.insertCell(2);
+        cell3.innerHTML = "<input type='text'  name='fname' />";
+
+        var cell4 = row.insertCell(3);
+        cell4.innerHTML =  "<input type='text'  name='phone' />";
+        }
+        function deleteRow(tableID) {
+        try {
+        var table = document.getElementById(tableID);
+        var rowCount = table.rows.length;
+
+        for(var i=1; i<rowCount; i++) {
+            var row = table.rows[i];
+            var chkbox = row.cells[0].childNodes[0];
+            if(null != chkbox && true == chkbox.checked) {
+                table.deleteRow(i);
+                rowCount--;
+                i--;
+            }
+        }
+        }catch(e) {
+            alert(e);
+        }
+    }
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
   </body>
 </html>
