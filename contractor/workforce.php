6dddca6d27b2fa90b8c31ec89225d82942b58cb2
lemasani <brianlemasan@gmail.com>
2023-08-25 10:13:06 +0300

    removed project details div

--- contractor/workforce.php
+++ contractor/workforce.php
@@ -25,41 +25,7 @@
   <?php include "..\includes\header1.php" ?>
   
     <div class="worforce-details">
-        <div class="row">
-            <div class="project-details">
-                <h4>Project details</h4>
-                <div class="row d-flex my-2">
-                    <div class="col-md-4 d-flex flex-column">
-                        <label for="project">Project name</label>
-                        <input type="text" id="project" >
-                    </div>
-                    <div class="col-md-4 d-flex flex-column">
-                        <label for="id">Project ID</label>
-                        <input type="text" id="id" >
-                    </div>
         
-                    <div class="col-md-4 d-flex flex-column">
-                        <label for="jurisdiction">jurisdiction</label>
-                        <input type="text"id="jurisdiction" >
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-3 d-flex flex-column">
-                        <label for="">Region</label>
-                        <input type="text">
-                    </div>
-                
-                    <div class="col-md-3 d-flex flex-column">
-                        <label for="">District</label>
-                        <input type="text" >
-                    </div>
-                    <div class="col-md-3 d-flex flex-column">
-                        <label for="">Street</label>
-                        <input type="text">
-                    </div>
-                </div>
-            </div>
-        </div>
 
         <div class="row">
             <h4 class="title my-3">Worforce Details</h4>
