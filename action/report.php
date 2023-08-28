979c3f94c0120af25f2fa02c5b646bf2a4296765
lemasani <brianlemasan@gmail.com>
2023-08-28 15:41:26 +0300

    report card template v1

---
+++ action/report.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+      <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Incident</title>
+
+    <style><?php include "..\Assets\css\incident.css"; ?></style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+
+    <?php include "..\includes\header.php"; ?> 
+    <div class="container">
+    <div class="card-section">
+        <div class="card shadow-sm" style="">
+        <div class="card-body">
+            <h5 class="card-title">Title: </h5>
+            
+            <p class="card-text my-2">Description: </p>
+            <div class="row d-flex my-5 ">
+                <div class="col-md-4">
+                    <p class="card-text">Region: </p>
+                </div>
+                <div class="col-md-4">
+                    <p class="card-text">District: </p>
+                </div>
+                <div class="col-md-4">
+                    <p class="card-text">Ward: </p>
+                </div>                
+            </div>
+            
+        </div>
+        </div>
+    </div>
+    </div>
+  
+
+    
+
+  <body>
+    
+  </body>
