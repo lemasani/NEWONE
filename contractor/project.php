6e832358ac495ff1c8da1069f73b1e1af2d25857
lemasani <brianlemasan@gmail.com>
2023-08-29 12:48:31 +0300

    contains initial code for the file

---
+++ contractor/project.php
@@ -0,0 +1,35 @@
+<div class="row">
+            <div class="project-details">
+                <h4>Project details</h4>
+                <div class="row d-flex my-2">
+                    <div class="col-md-4 d-flex flex-column">
+                        <label for="project">Project name</label>
+                        <input type="text" id="project" >
+                    </div>
+                    <div class="col-md-4 d-flex flex-column">
+                        <label for="id">Project ID</label>
+                        <input type="text" id="id" >
+                    </div>
+        
+                    <div class="col-md-4 d-flex flex-column">
+                        <label for="jurisdiction">jurisdiction</label>
+                        <input type="text"id="jurisdiction" >
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3 d-flex flex-column">
+                        <label for="">Region</label>
+                        <input type="text">
+                    </div>
+                
+                    <div class="col-md-3 d-flex flex-column">
+                        <label for="">District</label>
+                        <input type="text" >
+                    </div>
+                    <div class="col-md-3 d-flex flex-column">
+                        <label for="">Street</label>
+                        <input type="text">
+                    </div>
+                </div>
+            </div>
+        </div>
