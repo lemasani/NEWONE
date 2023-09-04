8dc09751d084e51a6504ca43f318f961f962bbaf
lemasani <brianlemasan@gmail.com>
2023-09-04 12:20:24 +0300

    connection to DB and validation

---
+++ contractor/connect.php
@@ -0,0 +1,10 @@
+<?php
+ $severname = "localhost";
+ $username = "root";
+ $password = "";
+$db_name = "newone";
+
+
+ $con = new mysqli( $severname, $username, $password, $db_name );
+
+ 
--- contractor/workforce.php
+++ contractor/workforce.php
@@ -1,3 +1,6 @@
+<?php 
+ include("connect.php");
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -23,6 +26,71 @@
   </head>
   <body>
   <?php include "..\includes\header1.php" ?>
+  <?php
+ $msg = '';
+ $national_id = '';
+$w_first_name = '';
+$w_last_name = ''; 
+$Phone = '';
+
+
+if($_SERVER['REQUEST_METHOD']=='POST') {
+
+
+$national_id = $_POST['national_id'];
+$w_first_name = $_POST['w_first_name'];
+$w_last_name = $_POST['w_last_name'];
+$Phone= $_POST['Phone'];
+
+
+if(empty($national_id) )
+{
+global $msg;
+$msg = '<div class="alert alert-danger mt-2 text-center">Give NIDA</div>';
+}
+
+elseif(empty($w_first_name))
+{
+  global $msg;
+$msg = '<div class="alert alert-danger mt-2 text-center">Provide First name</div>';
+}
+elseif(empty($w_last_name))
+{
+global $msg;
+$msg = '<div class="alert alert-danger mt-2 text-center">Provide last name</div>';
+}
+elseif(empty($Phone))
+{
+  global $msg;
+$msg = '<div class="alert alert-danger mt-2 text-center">Enter Phone number</div>';
+}
+
+else
+{
+  
+$sql = "insert into `workforce_tb`(national_id,w_first_name,w_last_name,Phone)
+values('$national_id','$w_first_name',' $w_last_name','$Phone')";
+
+
+
+$result = mysqli_query($con,$sql);
+
+if($result)
+{
+
+   $national_id = $_POST['national_id'];
+   $w_first_name = $_POST['w_first_name'];
+   $w_last_name = $_POST['w_last_name'];
+   $Phone = $_POST['Phone'];
+
+   
+   
+$msg = '<div class="alert alert-success mt-2 text-center">Data sent successfully</div>';
+
+}
+}
+}
+?>
   
     <div class="worforce-details">
         
@@ -40,22 +108,27 @@
                 <div class="row">
                     <div class="col-md-12 d-flex flex-column my-4">
                         <label for="NIDA">NIDA</label>
-                        <input type="text" name="nida" id="NIDA">
+                        <input type="text" name="national_id" id="NIDA">
                     </div>
                     <div class="col-md-4 d-flex flex-column">
                         <label for="fname">First Name</label>
-                        <input type="text" name="firstname" id="fname">
+                        <input type="text" name="w_first_name" id="fname">
                     </div>
                     <div class="col-md-4 d-flex flex-column">
                         <label for="lname">last Name</label>
-                        <input type="text" name="lastname" id="lname">
+                        <input type="text" name="w_last_name" id="lname">
                     </div>
                     <div class="col-md-4 d-flex flex-column">
                         <label for="phone">Phone No</label>
-                        <input type="text" name="phone" id="phone">
+                        <input type="text" name="Phone" id="phone">
                     </div>
-                    <div class="col-md-4 d-flex flex-column my-4">
+                    <div class="col-md-4 d-flex flex-column my-4 justify-content-center">
                         <input type="submit" value="Submit" id="submit" class="btn btn-success">
+                    </div>
+                    <div class="row d-flex justify-content-center">
+                        <div class="col-md-6">
+                        <?php echo $msg; ?>
+                        </div>
                     </div>
                 </div>
                 
