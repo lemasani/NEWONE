4f5e581f629f9a938e67ba73ef60f72e9f62a345
lemasani <brianlemasan@gmail.com>
2023-09-14 12:34:01 +0300

    template for view

---
+++ action/view.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View-more</title>
+    
+    <style><?php include "..\Assets\css\incident.css"; ?></style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<body>
+    <div class="report-result">
+        <div class="row">
+            <div class="col-md-4">
+                <span>Title: </span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <p id= "description"></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <span>Staff:</span>
+            </div>
+            <div class="col">
+                <span>Date&Time:</span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <span>Region:</span>
+            </div>
+            <div class="col-md-4">
+                <span>District:</span>
+            </div>
+            <div class="col-md-4">
+                <span>Ward:</span>
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
